525dd739cd99dbfb13f739ca0d48db1103e4e8cc
Nikita P. Shupeyko <webyneter@gmail.com>
2017-09-11 20:44:48 +0300

    Clean up code a bit

--- tests/test_comments_remover.py
+++ tests/test_comments_remover.py
@@ -3,9 +3,9 @@ from os.path import join, dirname
 from pytest import mark
 from python_humble_utils.commands import read_file, extract_file_name_with_extension
 
-from . import get_input_and_output_source_file_paths
 from comments_remover import Language, remove_comments_from_string, remove_comments_from_file, \
     DEFAULT_OUTPUT_FILE_PREFIX
+from . import get_input_and_output_source_file_paths
 
 
 def strip_spaces_and_linebreaks(s: str) -> str:
