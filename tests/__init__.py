83db529ffee0d9c6acba3966289e2446d7792814
Nikita P. Shupeyko <webyneter@gmail.com>
2017-09-12 13:21:56 +0300

    Encapsulate dir paths in get_input_and_output_source_file_paths
    QA: "2.2. [...] To finish, remove the unnecessary constants. [...]"

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,19 +4,16 @@ from typing import Tuple
 
 from comments_remover import Language
 
-TESTS_DIR_PATH: str = dirname(realpath(__file__))
-SOURCES_DIR_PATH: str = join(TESTS_DIR_PATH, 'sources')
-INPUT_SOURCE_FILE_PREFIX: str = 'input'
-OUTPUT_SOURCE_FILE_PREFIX: str = 'output'
-
 
 def get_input_and_output_source_file_paths(language: Language) -> Tuple[str, str]:
-    language_sources_dir_path = join(SOURCES_DIR_PATH, language.name)
+    tests_dir_path = dirname(realpath(__file__))
+    sources_dir_path = join(tests_dir_path, 'sources')
+    language_sources_dir_path = join(sources_dir_path, language.name)
     input_file_path = output_file_path = None
     for file_name in listdir(language_sources_dir_path):
-        if file_name.startswith(INPUT_SOURCE_FILE_PREFIX):
+        if file_name.startswith('input'):
             input_file_path = join(language_sources_dir_path, file_name)
-        if file_name.startswith(OUTPUT_SOURCE_FILE_PREFIX):
+        if file_name.startswith('output'):
             output_file_path = join(language_sources_dir_path, file_name)
         if input_file_path and output_file_path:
             return input_file_path, output_file_path
