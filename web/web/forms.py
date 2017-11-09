05ce903eea692d8c222298b32a11fc49dbaad499
Bernard Kagondu <bernard@savannahinformatics.com>
2017-11-09 20:23:52 +0300

    dynamically process the rest of the languages.

--- web/web/forms.py
+++ web/web/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 
+from web.comments_remover import getListOfLangs
+
+LANGUAGE_CHOICES = [
+    (each, each) for each in getListOfLangs()]
+
 
 class EditorForm(forms.Form):
     source_content = forms.CharField(
@@ -8,3 +13,7 @@ class EditorForm(forms.Form):
     results_content = forms.CharField(
         required=False,
         widget=forms.Textarea())
+    language = forms.ChoiceField(
+        required=False,
+        initial="HTML",
+        choices=LANGUAGE_CHOICES)
--- web/web/views.py
+++ web/web/views.py
@@ -4,29 +4,58 @@ from django.views import View
 from web.forms import EditorForm
 
 from web.comments_remover import (
-    Language, remove_comments_from_string)
+    Language, remove_comments_from_string, getListOfLangs)
 
 
 class WebView(View):
     template_name = "base.html"
+    form = EditorForm()
+    default_mode = 'htmlmixed'
+    default_language = 'JavaScript'
 
-    context = {}
+    def get_code_mirror_mode(self, language):
+        renamed = {
+            'c': 'clike',
+            'html': 'javascript',
+            'actionscript': self.default_mode,
+            'applescript': self.default_mode,
+            'bash': self.default_mode,
+            'csharp': self.default_mode
+        }
+        mode = language.lower()
+        return renamed[mode] if mode in renamed.keys() else mode
+
+    def get_context(self):
+        language = self.default_language
+        results_content = ''
+        if self.form.is_valid():
+            language = self.form.cleaned_data.get('language')
+            lang = Language.get_from_string(language)
+
+            source_content = self.form.cleaned_data.get('source_content')
+            results_content = (
+                remove_comments_from_string(
+                    source_content.replace('\r\n', '\n'),
+                    lang) if source_content else source_content)
+
+            self.form = EditorForm({
+                'source_content': source_content,
+                'results_content': results_content,
+                'language' : language})
+
+        context = {
+            'form': self.form,
+            'mode': self.get_code_mirror_mode(language)
+        }
+        return context
 
     def get(self, request, *args, **kwargs):
-        form = EditorForm()
-        self.context['form'] = form
-        return render(request, self.template_name, self.context)
+        context = self.get_context()
+        return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        source_content= request.POST['source_content']
-        language = Language.get_from_string('HTML')
-        results_content = remove_comments_from_string(
-            source_content, language)
-        form = EditorForm({
-            'source_content': source_content,
-            'results_content': results_content
-        })
-        self.context['form'] = form
-        response = render(request, self.template_name, self.context)
+        self.form = EditorForm(request.POST)
+        context = self.get_context()
+        response = render(request, self.template_name, context)
         response['X-XSS-Protection'] = 0 
         return response
